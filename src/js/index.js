728e6b6f2c17907fa1e33f718ed9e49457aeda7e
luckyant39 <141943730+luckyant39@users.noreply.github.com>
2024-03-10 16:18:20 +0200

    First commite

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import hello from './modules/hello';
+import mobileNav from './modules/mobile-nav.js';
+
+// import AirDatepicker from 'air-datepicker';
+// import 'air-datepicker/air-datepicker.css';
+
+// new AirDatepicker('#date')
+
+mobileNav();
+console.log(hello);
